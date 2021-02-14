a934a8410f38b22eaf8304647360db7118db84be
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-02-14 07:41:13 +0530

    Update 8specialstackgetmin0(1).cpp

--- stacks_queue/8specialstackgetmin0(1).cpp
+++ stacks_queue/8specialstackgetmin0(1).cpp
@@ -50,3 +50,50 @@ bool ispar(string x)
 
 //o(1) space and time
 // https://www.youtube.com/watch?v=QMlDCR9xyd8
+
+int mini  = 10000000;
+void push(stack<int>& s, int a){
+	// Your code goes here
+	if(s.size()==0){
+	    mini = a;
+	    s.push(a);
+	}
+	else if(a>mini){
+	    s.push(a);
+	}else{
+	    s.push(2*a-mini);
+	    mini = a;
+	}
+	
+	
+}
+
+bool isFull(stack<int>& s,int n){
+	// Your code goes here
+	return s.size()==n;
+}
+
+bool isEmpty(stack<int>& s){
+	// Your code goes here
+	return s.size()==0;
+}
+
+int pop(stack<int>& s){
+	// Your code goes here
+	if(s.size()==0){
+	    return -1;
+	}
+	else if(s.top()>mini)
+	s.pop();
+	else{
+	   mini = 2*mini - s.top();
+	   s.pop();
+	}
+	
+}
+
+int getMin(stack<int>& s){
+
+	return mini;
+	
+}
