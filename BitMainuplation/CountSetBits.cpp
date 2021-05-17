0a80acabd70ca1e3bdf52d3c393aea113a58e52c
Apoorv <47479228+Apoorv070@users.noreply.github.com>
2021-05-17 06:12:35 +0530

    Create CountSetBits.cpp

---
+++ BitMainuplation/CountSetBits.cpp
@@ -0,0 +1,9 @@
+    int setBits(int N) {
+        // Write Your Code here
+        int count = 0;
+        while(N>0){
+            count++;
+            N = N&(N-1);
+        }
+        return count;
+    }
